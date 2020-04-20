62c8f966e1366e72ba83c2c09dd795475efec885
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-04-20 18:53:47 +0900

    fix action use other repo

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ ENV REVIEWDOG_VERSION=v0.9.17
 RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
 RUN apk --no-cache add jq git
 
-COPY entrypoint.sh /entrypoint.sh
+WORKDIR /
+ENV PATH $PATH:/node_modules/.bin
+COPY package.json package-lock.json /
+RUN npm ci
+
+COPY entrypoint.sh /
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,15 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ ! -f "$(npm bin)/eslint" ]; then
-  npm install
-fi
-
-$(npm bin)/eslint --version
+eslint --version
 
 if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
   # Use jq and github-pr-review reporter to format result to include link to rule page.
-  $(npm bin)/eslint -f="json" ${INPUT_ESLINT_FLAGS:-'.'} \
+  eslint -f="json" ${INPUT_ESLINT_FLAGS:-'.'} \
     | jq -r '.[] | {filePath: .filePath, messages: .messages[]} | "\(.filePath):\(.messages.line):\(.messages.column):\(.messages.message) [\(.messages.ruleId)](https://eslint.org/docs/rules/\(.messages.ruleId))"' \
     | reviewdog -efm="%f:%l:%c:%m" -name="eslint" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  $(npm bin)/eslint -f="stylish" ${INPUT_ESLINT_FLAGS:-'.'} \
+  eslint -f="stylish" ${INPUT_ESLINT_FLAGS:-'.'} \
     | reviewdog -f="eslint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 fi
