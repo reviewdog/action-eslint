3bd14674618c677d476a9c870ac76a97e56e16dd
Laurence Armstrong <laurence.armstrong@hennge.com>
2020-05-08 10:30:27 +0900

    Change subdirectory logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,21 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-ESLINT_COMMAND="$(npm bin --prefix $INPUT_WORKDIR)/eslint"
+cd "${GITHUB_WORKSPACE}/${INPUT_DIRECTORY}" || exit 1
 
-if [ ! -f $ESLINT_COMMAND ]; then
-  npm install --prefix $INPUT_WORKDIR
+if [ ! -f "$(npm bin)/eslint" ]; then
+  npm install
 fi
 
-eval $ESLINT_COMMAND --version
+$(npm bin)/eslint --version
 
 if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
   # Use jq and github-pr-review reporter to format result to include link to rule page.
-  eval $ESLINT_COMMAND --resolve-plugins-relative-to $INPUT_WORKDIR \
-    -f="json" -c $INPUT_WORKDIR/.eslintrc.* ${INPUT_ESLINT_FLAGS:-$INPUT_WORKDIR} \
+  $(npm bin)/eslint -f="json" ${INPUT_ESLINT_FLAGS:-'.'} \
     | jq -r '.[] | {filePath: .filePath, messages: .messages[]} | "\(.filePath):\(.messages.line):\(.messages.column):\(.messages.message) [\(.messages.ruleId)](https://eslint.org/docs/rules/\(.messages.ruleId))"' \
     | reviewdog -efm="%f:%l:%c:%m" -name="eslint" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  eval $ESLINT_COMMAND --resolve-plugins-relative-to $INPUT_WORKDIR \
-    -f="stylish" -c $INPUT_WORKDIR/.eslintrc.* ${INPUT_ESLINT_FLAGS:-$INPUT_WORKDIR} \
+  $(npm bin)/eslint -f="stylish" ${INPUT_ESLINT_FLAGS:-'.'} \
     | reviewdog -f="eslint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 fi
