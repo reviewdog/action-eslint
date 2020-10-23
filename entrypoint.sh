5d0ea540da4b98ac087ce127faafd20e844fd0a9
haya14busa <haya14busa@gmail.com>
2020-10-23 12:13:24 +0000

    Simplify entrypoint.js

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,31 +5,13 @@ ESLINT_FORMATTER='/formatter.js'
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
-if [ ! -f "$(npm bin)/eslint" ]; then
-  npm install
-fi
+npx eslint --version
 
-$(npm bin)/eslint --version
-
-if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
-  # Use jq and github-pr-review reporter to format result to include link to rule page.
-  $(npm bin)/eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
-    | reviewdog -f=rdjson \
-        -name="${INPUT_TOOL_NAME}" \
-        -reporter=github-pr-review \
-        -filter-mode="${INPUT_FILTER_MODE}" \
-        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-        -level="${INPUT_LEVEL}" \
-        -tee \
-        ${INPUT_REVIEWDOG_FLAGS}
-else
-  # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  $(npm bin)/eslint -f="stylish" ${INPUT_ESLINT_FLAGS:-'.'} \
-    | reviewdog -f="eslint" \
-        -name="${INPUT_TOOL_NAME}" \
-        -reporter="${INPUT_REPORTER:-github-pr-check}" \
-        -filter-mode="${INPUT_FILTER_MODE}" \
-        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-        -level="${INPUT_LEVEL}" \
-        ${INPUT_REVIEWDOG_FLAGS}
-fi
+npx eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
+  | reviewdog -f=rdjson \
+      -name="${INPUT_TOOL_NAME}" \
+      -reporter=github-pr-review \
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS}
