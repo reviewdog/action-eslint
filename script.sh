3ff710d8b439e418618e8641104f4b2269b66222
haya14busa <haya14busa@gmail.com>
2021-02-07 12:30:52 +0000

    install reviewdog

--- script.sh
+++ script.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
+cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
+
+TEMP_PATH="$(mktemp -d)"
+PATH="${TEMP_PATH}:$PATH"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 ESLINT_FORMATTER="${GITHUB_ACTION_PATH}/eslint-formatter-rdjson/index.js"
 
-cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
+echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
+echo '::endgroup::'
 
 if [ ! -f "$(npm bin)/eslint" ]; then
+  echo '::group:: Running `npm install` to install eslint ...'
   npm install
+  echo '::endgroup::'
 fi
 
-$(npm bin)/eslint --version
+echo "eslint version:$($(npm bin)/eslint --version)"
 
+echo '::group:: Running eslint with reviewdog 🐶 ...'
 $(npm bin)/eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
   | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
@@ -19,3 +28,4 @@ $(npm bin)/eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS}
+echo '::endgroup::'
