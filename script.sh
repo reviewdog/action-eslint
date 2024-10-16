edb9f3ac67032ce61671adf4e7ab1ac34ef93e03
Manuel Fittko <manuel.fittko@sofatutor.com>
2024-10-16 18:44:59 +0200

    update

--- script.sh
+++ script.sh
@@ -7,31 +7,30 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 ESLINT_FORMATTER="${GITHUB_ACTION_PATH}/eslint-formatter-rdjson/index.js"
 
 echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
-# Install reviewdog to a known directory
+# Install reviewdog to the action's bin directory
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${GITHUB_ACTION_PATH}/bin"
 echo '::endgroup::'
 
 # Add reviewdog to PATH
 export PATH="${GITHUB_ACTION_PATH}/bin:${PATH}"
 
-echo '::group:: Running `npm install` to install ESLint and plugins ...'
-set -e
-# Change directory to your repository's workdir
-cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
-# Install ESLint and all required plugins specified in your .eslintrc.js
-npm install eslint eslint-plugin-react --no-save
-set +e
+echo '::group:: Installing ESLint and required plugins in the action directory...'
+# Install ESLint and required plugins in the action's directory
+npm install eslint eslint-plugin-react --prefix "${GITHUB_ACTION_PATH}" --no-save
 echo '::endgroup::'
 
-# Add node_modules binaries to PATH
-export PATH="${GITHUB_WORKSPACE}/${INPUT_WORKDIR}/node_modules/.bin:${PATH}"
+# Add action's node_modules binaries to PATH
+export PATH="${GITHUB_ACTION_PATH}/node_modules/.bin:${PATH}"
 
 echo '::group:: Running ESLint with reviewdog 🐶 ...'
 eslint_output=$(mktemp)
-# Run ESLint using the formatter
-eslint -f="${ESLINT_FORMATTER}" "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" > "$eslint_output"
+# Run ESLint with --resolve-plugins-relative-to pointing to the action's directory
+eslint --resolve-plugins-relative-to "${GITHUB_ACTION_PATH}" -f="${ESLINT_FORMATTER}" "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" > "$eslint_output"
 
-# Use reviewdog from PATH
+# Change directory to your repository's workdir
+cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
+
+# Use reviewdog from the action's bin directory
 cat "$eslint_output" | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-review}" \
