ba7a0676abba983637d2c3651bff5fe427e9ea73
Manuel Fittko <manuel.fittko@sofatutor.com>
2024-10-16 18:16:15 +0200

    install in .bin

--- script.sh
+++ script.sh
@@ -2,12 +2,11 @@
 
 cd "${GITHUB_ACTION_PATH}" || exit 1
 
-TEMP_PATH="$(mktemp -d)"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 ESLINT_FORMATTER="${GITHUB_ACTION_PATH}/eslint-formatter-rdjson/index.js"
 
 echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
-curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s
 echo '::endgroup::'
 
 # DEBUG
@@ -23,6 +22,10 @@ echo '::endgroup::'
 export PATH="${GITHUB_ACTION_PATH}/.bin:${GITHUB_ACTION_PATH}/node_modules/.bin:$PATH"
 # list all installed packages
 npm list -g --depth=0
+
+# debug
+which eslint
+which reviewdog
 
 echo '::group:: Running eslint with reviewdog 🐶 ...'
 eslint -f="${ESLINT_FORMATTER}" ${INPUT_ESLINT_FLAGS:-'.'} \
